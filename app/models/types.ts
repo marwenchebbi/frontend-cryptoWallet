ec896f111d969aad46e8e04e36ab92b5fd172ea3
marwenchebbi <marwenchebbi111@gmail.com>
2025-06-06 20:14:11 +0100

    Update types.ts

--- app/models/types.ts
+++ app/models/types.ts
@@ -1,5 +1,5 @@
 // types/constants.ts
-export const IP_ADDRESS = "192.168.137.1";
+export const IP_ADDRESS = ;
 
 //stripe API
-export const PULISHABLE_KEY = 'pk_test_51RNDNxPxKLJDnkwPg6AMgWeQ6i07K6k1oo3ncw4a7ztOZzKurRQdsFgtQP7TFTiLTnCBB9y6qnoHD3jwluSYkSle00qiILGZjd'
+export const PULISHABLE_KEY = 
