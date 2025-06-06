3cbba7f4894021cd9209e63255cb1638db530ef5
marwenchebbi <marwenchebbi111@gmail.com>
2025-06-06 20:14:42 +0100

    Update types.ts

--- app/models/types.ts
+++ app/models/types.ts
@@ -1,9 +1,5 @@
 // types/constants.ts
-export const IP_ADDRESS = "192.168.1.14";
+export const IP_ADDRESS = 
 
 //stripe API
-export const PULISHABLE_KEY = 'pk_test_51RNDNxPxKLJDnkwPg6AMgWeQ6i07K6k1oo3ncw4a7ztOZzKurRQdsFgtQP7TFTiLTnCBB9y6qnoHD3jwluSYkSle00qiILGZjd'
-
-
-
-'https://trello.com/power-ups/683e076512a79a1cbbbc3800/edit/api-key'
+export const PULISHABLE_KEY = 
